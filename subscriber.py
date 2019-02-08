dad922fd060cd04cca8ec980a140c946c0b4c029
Sagar <sagar.k.shah@vanderbilt.edu>
2019-02-08 01:51:28 -0600

    Subscriber updated

--- subscriber.py
+++ subscriber.py
@@ -1,7 +1,15 @@
 from CS6382 import ToBroker
 import sys
 
+
+def message_handler1(topic, value):
+    print("In message handler callback\nTopic: {}, Value: {}".format(topic, value))
+
+
 Broker_API = ToBroker()
-topic = sys.argv[1]
-Broker_API.register_sub("{}".format(topic))
-Broker_API.notify("{}".format(topic))
+topic1 = sys.argv[1]
+# topic2 = sys.argv[2]
+Broker_API.register_sub("{}".format(topic1))
+# Broker_API.register_sub("{}".format(topic2))
+Broker_API.notify("{}".format(topic1), message_handler1)
+print("After Notify")  # This should not happen
