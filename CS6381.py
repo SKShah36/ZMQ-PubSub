d3a93b2d84d5a3adce6ef84ecd77a941e75540de
Sagar <sagar.k.shah@vanderbilt.edu>
2019-02-23 00:58:54 -0600

    Pipeline created

--- CS6381.py
+++ CS6381.py
@@ -1,85 +1,146 @@
 from threading import Thread, Lock
 from queue import Queue
+from kazoo.client import KazooClient
+from kazoo.client import KazooState
+import kazoo
 import time
 import zmq
 import hashlib
 import random
 import os
 import configparser
+import sys
 
 
 class ToBroker:
     def __init__(self):
-        config = configparser.ConfigParser()
-        config.read('config.ini')
-        self.ports = config['PORT']
-        self.ip = config['IP']
+        hosts = '127.0.0.1:2181'
+
+        try:
+            self.zk = KazooClient(hosts)
+            self.zk.add_listener(self.state_listener)
+            self.zk.start()
+
+            try:
+                self.zk.ensure_path("/app/publishers")
+                self.zk.ensure_path("/app/subscribers")
+
+            except kazoo.exceptions.NodeExistsError:
+                print("Node exists")
+
+            except kazoo.exceptions.NoNodeError:
+                print("There's no such node yet")
+
+        except:
+            print("Connection to the zookeeper cannot be established.\nEnsure zookeeper is running.\nExiting...")
+            sys.exit(0)
 
         context = zmq.Context()
 
         self.pub_socket = context.socket(zmq.DEALER)
-        self.pub_socket.connect("tcp://{}:{}".format(self.ip['BROKER_IP'], self.ports['RECEIVE']))
-
         self.sub_socket = context.socket(zmq.DEALER)
-        self.sub_socket.connect("tcp://{}:{}".format(self.ip['BROKER_IP'], self.ports['SEND']))
 
-        self.reqp_socket = context.socket(zmq.DEALER)
-        self.reqp_socket.connect("tcp://{}:{}".format(self.ip['BROKER_IP'], self.ports['REGISTER_PUBLISHER']))
+        self.broker_ip = ""
+        self.connection_port_pub = ""
+        self.connection_port_sub = ""
 
-        self.reqs_socket = context.socket(zmq.DEALER)
-        self.reqs_socket.connect("tcp://{}:{}".format(self.ip['BROKER_IP'], self.ports['REGISTER_SUBSCRIBER']))
-
-        self.heartbeat_socket = context.socket(zmq.DEALER)
-        self.heartbeat_socket.connect("tcp://{}:{}".format(self.ip['BROKER_IP'], self.ports['HEARTBEAT']))
-
-        self.pub_id = ""
-        self.sub_id = ""
-
-    # Register publisher with broker
-    def register_pub(self, topic):
         # Set publisher id
-        hash_obj = hashlib.md5()
-        hash_obj.update("{}{}".format(random.randint(1, 99999), os.getpid()).encode())
-        self.pub_id = hash_obj.hexdigest()
+        hash_obj_pub = hashlib.md5()
+        hash_obj_pub.update("{}{}".format(random.randint(1, 99999), os.getpid()).encode())
+        self.pub_id = hash_obj_pub.hexdigest()
 
-        # Connect and send registration message to broker
-        self.reqp_socket.send_string("{}-{}".format(topic, self.pub_id))
+        # Set subscriber id
+        hash_obj_sub = hashlib.md5()
+        hash_obj_sub.update("{}{}".format(random.randint(1, 99999), os.getpid()).encode())
+        self.sub_id = hash_obj_sub.hexdigest()
 
-        # Setup heartbeat thread
-        heartbeat_thread = Thread(target=self.heartbeat, args=(self.pub_id,))
-        heartbeat_thread.daemon = True
-        heartbeat_thread.start()
+    def state_listener(self, state):
+        if state == KazooState.LOST:
+            print("Current state is now = LOST")
+        elif state == KazooState.SUSPENDED:
+            print("Current state is now = SUSPENDED")
+        elif state == KazooState.CONNECTED:
+            print("Current state is now = CONNECTED")
+        else:
+            print("Current state now = UNKNOWN !! Cannot happen")
 
-        # Receive registration confirmation
-        return_msg = self.reqp_socket.recv()
-        return_msg = return_msg.decode('utf-8')
-        print("Return message: {}".format(return_msg))
+    # Register publisher with zookeeper
+    def register_pub(self, topic):
+        # Create a publisher node
+        try:
+            self.zk.ensure_path("/app/publishers/{}".format(self.pub_id))
+            self.zk.create("/app/publishers/{}/{}".format(self.pub_id, topic), "{}".format(topic).encode('utf-8'),
+                           ephemeral=True)
+            print("Subscriber has already been registered with the topic".format(topic))
+            self.connect2broker(0)
+
+        except kazoo.exceptions.NodeExistsError:
+            self.zk.delete("/app/publishers/{}".format(self.pub_id))
+            print("Publisher has already been registered with the topic")
 
     # Register subscriber with broker
     def register_sub(self, topic):
-        # Set subscriber id
-        hash_obj = hashlib.md5()
-        hash_obj.update("{}{}".format(random.randint(1, 99999), os.getpid()).encode())
-        self.sub_id = hash_obj.hexdigest()
 
-        # Connect and send registration message to broker
-        self.reqs_socket.send_string("{}-{}".format(topic, self.sub_id))
+        # Create a subscriber node
+        try:
+            self.zk.ensure_path("/app/subscribers/{}".format(self.sub_id))
+            self.zk.create("/app/subscribers/{}/{}".format(self.sub_id, topic), "{}".format(topic).encode('utf-8'),
+                           ephemeral=True)
+            print("Subscriber {} registered for topic {}".format(self.pub_id, topic))
+            self.connect2broker(1)
 
-        # Setup heartbeat thread
-        heartbeat_thread = Thread(target=self.heartbeat, args=(self.sub_id,))
-        heartbeat_thread.daemon = True
-        heartbeat_thread.start()
+        except kazoo.exceptions.NodeExistsError:
+            self.zk.delete("/app/publishers/{}".format(self.sub_id))
+            print("Subscriber has already been registered with the topic".format(topic))
 
-        # Receive registration confirmation
-        return_msg = self.reqs_socket.recv()
-        return_msg = return_msg.decode('utf-8')
-        print("Return message: {}".format(return_msg))
+    def connect2broker(self, i):
+        try:
+            self.broker_ip = self.zk.get("/app/broker/leader/ip")
+            self.broker_ip = self.broker_ip[0].decode('utf-8')
+            print("Current broker ip", self.broker_ip)
+
+            self.connection_port_pub = self.zk.get("/app/broker/leader/publish_handle")
+            self.connection_port_pub = self.connection_port_pub[0].decode('utf-8')
+            print("Current pub port", self.connection_port_pub)
+
+            self.connection_port_sub = self.zk.get("/app/broker/leader/subscribe_handle")
+            self.connection_port_sub = self.connection_port_sub[0].decode('utf-8')
+
+        except kazoo.exceptions.NoNodeError:
+            print("There's no broker available right now")
+        if i == 0:
+            self.pub_socket.connect("tcp://{}:{}".format(self.broker_ip, self.connection_port_pub))
+        elif i == 1:
+            self.sub_socket.connect("tcp://{}:{}".format(self.broker_ip, self.connection_port_sub))
 
     # Connect and publish messages
     def publish(self, topic, value):
-        top_val = "{},{}-{}:{}".format(self.pub_id, topic, value, time.time())
-        print("Publishing {}".format(top_val))
-        self.pub_socket.send_string(top_val)
+        try:
+            if not self.zk.exists("/app/publishers/{}".format(self.pub_id)):
+                print("Publisher {} isn't registered".format(self.pub_id))
+                sys.exit(0)
+            elif topic not in self.zk.get_children("/app/publishers/{}".format(self.pub_id)):
+                print("{} hasn't been registered for the publisher id {}. Register before publishing".format(topic, self.pub_id))
+
+            else:
+                top_val = "{},{}-{}:{}".format(self.pub_id, topic, value, time.time())
+                print("Publishing {}".format(top_val))
+                try:
+                    self.pub_socket.send_string(top_val)
+
+                except zmq.ZMQError:
+                    print("Something is wrong with the connection")
+
+        except KeyboardInterrupt:
+            self.zk.delete("/app/publishers/{}".format(self.pub_id)) # This is working but what happens if there's a power failure
+            self.zk.stop()
+
+    def watch_broker(self, events=None):
+        self.pub_socket.disconnect("tcp://{}:{}".format(self.broker_ip, self.connection_port_pub))
+        self.sub_socket.disconnect("tcp://{}:{}".format(self.broker_ip, self.connection_port_sub))
+
+        self.connect2broker(0)
+        self.connect2broker(1)
 
     # Check for notification from broker, if any
     def publish_helper(self):
@@ -123,6 +184,8 @@ class ToBroker:
 
         except KeyboardInterrupt:
             f.close()
+            self.zk.delete("/app/subscribers/{}".format(self.sub_id))
+            self.zk.stop()
 
     def heartbeat(self, identity):
         while True:
@@ -136,83 +199,118 @@ class ToBroker:
 
 
 class FromBroker:
-
-    def __init__(self):
+    def __init__(self, my_ip='127.0.0.1'):
         config = configparser.ConfigParser()
         config.read('config.ini')
-        ports = config['PORT']
+        self.ports = config['PORT']
+        self.ip = config['IP']['BROKER_IP']
+
+        self.receiver_socket = ""
+        self.sender_socket = ""
+        self.heartbeat_socket = ""
+        self.queue = ""
+
+        hosts = '127.0.0.1:2181'
+
+        try:
+            self.zk = KazooClient(hosts)
+            self.zk.add_listener(self.state_listener)
+            self.zk.start()
+
+            try:
+                self.zk.ensure_path("/app/broker/leader")
+
+            except kazoo.exceptions.NodeExistsError:
+                print("Node exists")
+
+            except kazoo.exceptions.NoNodeError:
+                print("There's no such node yet")
+
+        except:
+            print("Connection to the zookeeper cannot be established.\nEnsure zookeeper is running.\nExiting...")
+            sys.exit(0)
+
+    def state_listener(self, state):
+        if state == KazooState.LOST:
+            print("Current state is now = LOST")
+        elif state == KazooState.SUSPENDED:
+            print("Current state is now = SUSPENDED")
+        elif state == KazooState.CONNECTED:
+            print("Current state is now = CONNECTED")
+        else:
+            print("Current state now = UNKNOWN !! Cannot happen")
+
+    def create_broker(self, events=None):
+        try:
+            self.zk.create("/app/broker/leader/ip", "{}".format(self.ip).encode('utf-8'), ephemeral=True)
+            self.zk.create("/app/broker/leader/publish_handle", "{}".format(self.ports['RECEIVE']).encode('utf-8'), ephemeral=True)
+            self.zk.create("/app/broker/leader/subscribe_handle", "{}".format(self.ports['SEND']).encode('utf-8'), ephemeral=True)
+            print("Broker created with IP {}".format(self.ip))
+
+        except kazoo.exceptions.NodeExistsError:
+            self.zk.exists("/app/broker/leader/ip", watch=self.create_broker)
+
+    def setup_broker(self):
+
         broker_context = zmq.Context()
-        self.pub_reg_socket = broker_context.socket(zmq.ROUTER)   # Socket to register request from publisher
-        self.pub_reg_socket.bind("tcp://*:{}".format(ports['REGISTER_PUBLISHER']))
-        self.sub_reg_socket = broker_context.socket(zmq.ROUTER)   # Socket to register request from subscriber
-        self.sub_reg_socket.bind("tcp://*:{}".format(ports['REGISTER_SUBSCRIBER']))
-        self.sender_socket = broker_context.socket(zmq.ROUTER)    # Socket to send messages from publisher to subscriber
-        self.sender_socket.bind("tcp://*:{}".format(ports['SEND']))
+
         self.receiver_socket = broker_context.socket(zmq.ROUTER)  # Socket to receive messages from publisher
-        self.receiver_socket.bind("tcp://*:{}".format(ports['RECEIVE']))
-        self.heartbeat_socket = broker_context.socket(zmq.ROUTER)
-        self.heartbeat_socket.bind("tcp://*:{}".format(ports['HEARTBEAT']))
-        self.discovery = {"publishers": {}, "subscribers": {}, "id": {}}
+        self.receiver_socket.bind("tcp://*:{}".format(self.ports['RECEIVE']))
+
+        self.sender_socket = broker_context.socket(zmq.ROUTER)  # Socket to send messages from publisher to subscriber
+        self.sender_socket.bind("tcp://*:{}".format(self.ports['SEND']))
+
         self.queue = Queue()
 
-    def register_publisher_handler(self):
+    def publisher_handler(self):
 
         while True:
-            print("Register Publisher waiting for message")
-            address, top_id = self.pub_reg_socket.recv_multipart()
-            top_id = top_id.decode('utf-8')
-            topic, pub_id = top_id.split('-')
-            self.discovery["publishers"][pub_id] = {"address": address, "topic": topic}
-            self.discovery["id"][pub_id] = time.time()
-            # print(self.discovery)
-            self.pub_reg_socket.send_multipart(
-                [address, "{} is successfully registered".format(pub_id).encode('utf-8')])
-            time.sleep(3)
-
-    def register_subscriber_handler(self):
-        # Keeps listening for subscribers
-        while True:
-            print("Register Subscriber waiting for message")
-            address, top_id = self.sub_reg_socket.recv_multipart()
-            top_id = top_id.decode('utf-8')
-            topic, sub_id = top_id.split('-')
-
-            self.discovery["subscribers"][sub_id] = {"address": address, "topic": topic}
-            self.discovery["id"][sub_id] = time.time()
-            print(self.discovery)
-            self.sub_reg_socket.send_multipart(
-                [address, "{} is successfully registered".format(address).encode('utf-8')])
-            time.sleep(3)
-
-    def receive_handler(self):
-
-        while True:
+            # print("Waiting for messages in publish handler")
             address, msg = self.receiver_socket.recv_multipart()
             msg = msg.decode('utf-8')
-            pub_id, top_val = msg.split(',')
             print("Received message: {}".format(msg))
-            if pub_id not in self.discovery["publishers"].keys():
-                print("Publisher isn't registered")
-                self.receiver_socket.send_multipart([address, b"Publisher isn't registered. Register before publishing"])
+            pub_id, top_val = msg.split(',')
+            topic, value = top_val.split('-')
+
+            if self.zk.exists("/app/publishers/{}/{}".format(pub_id, topic)):
+                try:
+                    self.zk.create("/app/publishers/{}/address".format(pub_id), address, ephemeral=True)
+                except kazoo.exceptions.NodeExistsError:
+                    pass
+                except kazoo.exceptions.NoNodeError:
+                    time.sleep(1)
+                    continue
+
+                self.queue.put(msg)
+
             else:
-                self.discovery["publishers"][pub_id]["address"] = address
-                self.queue.put(top_val)
+                continue
 
     def send_handler(self):
 
         while True:
-            if self.discovery["subscribers"]:
-                top_val = self.queue.get()
-                topic, val = top_val.split('-')
 
-                for sub_id in self.discovery["subscribers"].keys():
-                    if topic in self.discovery["subscribers"][sub_id]["topic"]:
-                        address = self.discovery["subscribers"][sub_id]["address"]
-                        self.sender_socket.send_multipart([address, top_val.encode('utf-8')])
+            msg = self.queue.get()
+            pub_id, top_val = msg.split(',')
+            topic, val = top_val.split('-')
+
+            if self.zk.exists("/app/subscribers/"):
+                for sub_id in self.zk.get_children("/app/subscribers"):
+                    if topic in self.zk.get_children("/app/subscribers/{}".format(sub_id)):
+
+                        if self.zk.exists("/app/subscribers/{}/address".format(sub_id)):
+                            address = self.zk.get("/app/subscribers/{}/address".format(sub_id))
+                            address = address[0]
+                            print("Sending on address", address)
+                            self.sender_socket.send_multipart([address, top_val.encode('utf-8')])
+
+                        else:
+                            time.sleep(1)
+                            continue
 
             else:
                 print("No subscribers yet")
-                time.sleep(5)
+                time.sleep(2)
                 continue
 
     def update_subscriber_address(self):
@@ -221,78 +319,33 @@ class FromBroker:
             address, top_id = self.sender_socket.recv_multipart()
             top_id = top_id.decode('utf-8')
             sub_id, topic = top_id.split(',')
-            if sub_id in self.discovery["subscribers"].keys() and topic in self.discovery["subscribers"][sub_id]["topic"]:
-                self.discovery["subscribers"][sub_id]["address"] = address
-
-    def is_client_dead(self):
-        while True:
-            if self.discovery["id"]:
-                delete_list = []
-                lock = Lock()
-                lock.acquire()
+            print("Subs address{}".format(address))
+            if self.zk.exists("/app/subscribers/{}/{}".format(sub_id, topic)):
+                print("Subid added")
                 try:
-                    for identity in self.discovery["id"].keys():
-                        last_heartbeat = self.discovery["id"][identity]
-                        time_diff = int(round(time.time()-last_heartbeat))
-                        threshold = 60 + 10*(len(self.discovery["id"]))
-                        if time_diff > threshold:
-                            delete_list.append(identity)
-                finally:
-                    lock.release()
-                for identity in delete_list:
-                    try:
-                        self.discovery["publishers"].pop(identity)
-                    except KeyError:
-                        pass
-                    try:
-                        self.discovery["subscribers"].pop(identity)
-                    except KeyError:
-                        pass
-                    try:
-                        self.discovery["id"].pop(identity)
-                    except KeyError:
-                        pass
-                    print("{} is dead. Removed!".format(identity))
-
-            else:
-                print("There's no client alive")
-                time.sleep(5)
-
-    def update_heartbeat(self):
-            address, heartbeat_id = self.heartbeat_socket.recv_multipart()
-            heartbeat_recv_time = time.time()
-            heartbeat_id = heartbeat_id.decode('utf-8')
-
-            # Check and update last heartbeat
-            if heartbeat_id in self.discovery["id"].keys():
-                self.discovery["id"][heartbeat_id] = heartbeat_recv_time
+                    self.zk.create("/app/subscribers/{}/address".format(sub_id), address, ephemeral=True)
+                except kazoo.exceptions.NodeExistsError:
+                    print("Cannot use notify more than once")
 
     def run(self):
 
-        t1 = Thread(target=self.register_publisher_handler)
-        t2 = Thread(target=self.register_subscriber_handler)
-        t3 = Thread(target=self.receive_handler)
-        t4 = Thread(target=self.send_handler)
-        t5 = Thread(target=self.update_subscriber_address)
-        t6 = Thread(target=self.is_client_dead)
-        t7 = Thread(target=self.update_heartbeat)
+        self.create_broker()
+        self.setup_broker()
+        t1 = Thread(target=self.publisher_handler)
+        t2 = Thread(target=self.send_handler)
+        t3 = Thread(target=self.update_subscriber_address)
+
         t1.start()
         t2.start()
         t3.start()
-        t4.start()
-        t5.start()
-        t6.start()
-        t7.start()
+
         while True:
             print("...")
             time.sleep(3)
+
         t1.join()
         t2.join()
         t3.join()
-        t4.join()
-        t5.join()
-        t6.join()
-        t7.join()
 
 
 
--- publisher.py
+++ publisher.py
@@ -1,14 +1,10 @@
 from CS6381 import ToBroker
-import random
 import time
 import sys
 
 Broker_API = ToBroker()
-topic1 = sys.argv[1]
-# topic2 = sys.argv[2]
-Broker_API.register_pub("{}".format(topic1))
-# Broker_API.register_pub("{}".format(topic2))
+
+Broker_API.register_pub("Temperature")
 while True:
-    Broker_API.publish("{}".format(topic1), random.randint(1, 1000))
-    # Broker_API.publish("{}".format(topic2), random.randint(1, 1000))
+    Broker_API.publish("Temperature", 15)
     time.sleep(5)
